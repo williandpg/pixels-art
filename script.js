0a7d393d8f660daddeddc2273a962339776f81b7
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-17 21:03:36 -0300

    Iniciando seção 10

--- script.js
+++ script.js
@@ -9,14 +9,31 @@ const generateColor = () => {
 
 const generateColorPalette = () => {
   const colors = document.querySelectorAll('.color');
+  const colorPalette = [];
   for (let index = 0; index < 4; index += 1) {
+    const color = generateColor();
+    colorPalette.push(color);
     colors[index].style.backgroundColor = generateColor();
     colors[0].style.backgroundColor = 'black';
   }
-  localStorage.setItem('colorPalette', JSON.stringify(colors));
+  localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
 };
 const buttonRandomColor = document.querySelector('#button-random-color');
 buttonRandomColor.addEventListener('click', generateColorPalette);
+
+window.onload = () => {
+  const storedColorPalette = localStorage.getItem('colorPalette');
+  if (storedColorPalette) {
+    const colors = document.querySelectorAll('.color');
+    const colorPalette = JSON.parse(storedColorPalette);
+    for (let index = 0; index < colors.length; index += 1) {
+      colors[index].style.backgroundColor = colorPalette[index];
+      colors[0].style.backgroundColor = 'black';
+    }
+  } else {
+    generateColorPalette();
+  }
+};
 
 const pixelBoard = document.getElementById('pixel-board');
 const createPixels = () => {
@@ -45,6 +62,26 @@ const selectColor = (event) => {
 };
 colorPalette.addEventListener('click', selectColor);
 
-window.onload = () => {
-  generateColorPalette();
+const colors = document.querySelectorAll('.color');
+const pixels = document.querySelectorAll('.pixel');
+const selectColors = (event) => {
+  colors.forEach((colorHexa) => colorHexa.classList.remove('selected'));
+  event.target.classList.add('selected');
+  localStorage.setItem('selectedColor', event.target.style.backgroundColor);
 };
+const fillPixel = (event) => {
+  if (event.target.classList.contains('pixel')) {
+    // eslint-disable-next-line no-param-reassign
+    event.target.style.backgroundColor = localStorage.getItem('selectedColor');
+  }
+};
+colors.forEach((colorHexa) => colorHexa.addEventListener('click', selectColors));
+pixels.forEach((pixel) => pixel.addEventListener('click', fillPixel));
+const storedColor = localStorage.getItem('selectedColor');
+if (storedColor) {
+  colors.forEach((colorHexa) => {
+    if (colorHexa.style.backgroundColor === storedColor) {
+      colorHexa.classList.add('selected');
+    }
+  });
+}
